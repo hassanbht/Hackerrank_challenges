e4a5b426b26c34d555d3b2edee678bdf461b13e4
Hassan <hassan_bht@yahoo.com>
2023-09-20 22:38:44 +0330

    complate divisibleSumPairs

--- HackerRankSolutions.DivisibleSumPairs/Program.cs
+++ HackerRankSolutions.DivisibleSumPairs/Program.cs
@@ -1,1 +1,60 @@
-﻿
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'divisibleSumPairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY ar
+     */
+
+    public static int divisibleSumPairs(int n, int k, List<int> ar)
+    {
+        int count = 0;
+        for(int i = 0; i < n;i++)
+        {
+            for(int j = i+1;j<n;j++)
+            {
+                if ((ar[i] + ar[j]) % k == 0)
+                    count++;
+            }
+        }
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {      
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.divisibleSumPairs(n, k, ar);
+
+        Console.WriteLine(result);
+    }
+}
