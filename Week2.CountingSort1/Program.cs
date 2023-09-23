6dd8a75c9b13702425a2fead79f0aafae73e1497
Hassan <hassan_bht@yahoo.com>
2023-09-23 23:16:48 +0330

    compalte countingValleys

--- Week2.CountingSort1/Program.cs
+++ Week2.CountingSort1/Program.cs
@@ -1,17 +1,45 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿/*
+ Comparison Sorting
 
+Quicksort usually has a running time of n x log(n), but is there an algorithm that can sort even faster? 
+In general, this is not possible. Most sorting algorithms are comparison sorts, i.e.
+they sort a list just by comparing the elements to one another. 
+A comparison sort algorithm cannot beat n x log(n) (worst-case) running time, since n x log(n) 
+represents the minimum number of comparisons needed to know where to place each element. For more details,
+you can see these notes (PDF).
+
+Alternative Sorting
+
+Another sorting method, the counting sort, does not require comparison.
+Instead, you create an integer array whose index range covers the entire range of values in your array to sort.
+Each time a value occurs in the original array, you increment the counter at that index.
+At the end, run through your counting array, printing the value of each non-zero valued index that number of times.
+
+Example
+
+arr = [1,1,1,3,2,1]
+All of the values are in the range [0...3], 
+so create an array of zeros result = [0,0,0,0]. 
+The results of each iteration follow:
+
+i   arr[i]  result
+0   1   [0, 1, 0, 0]
+1   1   [0, 2, 0, 0]
+2   3   [0, 2, 0, 1]
+3   2   [0, 2, 1, 1]
+4   1   [0, 3, 1, 1]
+The frequency array is [0,3,1,1]. These values can be used to create the sorted array as well:
+sorted = [1,1,1,2,3].
+
+Note
+
+For this exercise, always return a frequency array with 100 elements.
+The example above shows only the first 4 elements, the remainder being zeros.
+
+Challenge
+
+Given a list of integers, count and return the number of times each value appears as an array of integers.
+*/
 class Result
 {
 
@@ -22,14 +50,11 @@ class Result
      * The function accepts INTEGER_ARRAY arr as parameter.
      */
 
-    public static List<int> countingSort(List<int> arr)
+public static List<int> countingSort(List<int> arr)
     {
-        int[] counts = new int[100];
-        foreach (int num in arr)
-        {
-            counts[num]++;
-        }
-        return counts.ToList();
+        int[] result = new int[100];
+        arr.ForEach(a => result[a]++);
+        return result.ToList();
     }
 
 }
@@ -37,7 +62,7 @@ class Result
 class Solution
 {
     public static void Main(string[] args)
-    {       
+    {
         int n = Convert.ToInt32(Console.ReadLine().Trim());
 
         List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
---
+++ Week2.CountingValleys/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+    An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps,
+    for every step it was noted if it was an uphill, , or a downhill,  step. Hikes always start and end at sea level,
+    and each step up or down represents a  unit change in altitude. We define the following terms:
+
+    A mountain is a sequence of consecutive steps above sea level,
+    starting with a step up from sea level and ending with a step down to sea level.
+    A valley is a sequence of consecutive steps below sea level,
+    starting with a step down from sea level and ending with a step up to sea level.
+    Given the sequence of up and down steps during a hike,
+    find and print the number of valleys walked through.
+*/
+class Result
+{
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+public static int countingValleys(int steps, string path)
+    {
+        int level = 0; // Current altitude level
+        int valleyCount = 0; // Number of valleys walked through
+        bool isInValley = false; // Flag to track if currently in a valley
+
+        foreach (char step in path)
+        {
+            if (step == 'U')
+            {
+                level++; // Step up
+            }
+            else if (step == 'D')
+            {
+                level--; // Step down
+            }
+
+            if (level < 0 && !isInValley)
+            {
+                isInValley = true; // Entered a valley
+            }
+
+            if (level == 0 && isInValley)
+            {
+                valleyCount++; // Exited a valley
+                isInValley = false;
+            }
+        }
+        return valleyCount;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        Console.WriteLine(result);
+    }
+}
