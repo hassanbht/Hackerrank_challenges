37b89286b4ef38611197ce31e2506de04e7c073a
Hassan <hassan_bht@yahoo.com>
2023-11-10 23:10:25 +0330

    Complate New Year Chaos hackerrank solution c#

---
+++ Week7.New_Year_Chaos/Program.cs
@@ -0,0 +1,73 @@
+﻿/*
+ New Year Chaos hackerrank solution c#
+ It is New Year's Day and people are in line for the Wonderland rollercoaster ride.
+Each person wears a sticker indicating their initial position in the queue from 1 to n.
+Any person can bribe the person directly in front of them to swap positions,
+but they still wear their original sticker. One person can bribe at most two others.
+
+Determine the minimum number of bribes that took place to get to a given queue order.
+Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
+function Description
+minimumBribes has the following parameter(s):
+int q[n]: the positions of the people after all bribes
+Returns
+No value is returned. Print the minimum number of bribes necessary or Too chaotic if someone has bribed 
+more than 2 people.
+Input Format
+The first line contains an integer t, the number of test cases.
+Each of the next  pairs of lines are as follows:
+- The first line contains an integer t, the number of people in the queue
+- The second line has n space-separated integers describing the final state of the queue.
+*/
+
+class Result
+{
+
+    /*
+     * Complete the 'minimumBribes' function below.
+     *
+     * The function accepts INTEGER_ARRAY q as parameter.
+     */
+
+    public static void minimumBribes(List<int> q)
+    {
+        int bribes = 0;
+
+        for (int i = 0; i < q.Count; i++)
+        {
+            if (q[i] - (i + 1) > 2)
+            {
+                Console.WriteLine("Too chaotic");
+                return;
+            }
+
+            for (int j = Math.Max(0, q[i] - 2); j < i; j++)
+            {
+                if (q[j] > q[i])
+                {
+                    bribes++;
+                }
+            }
+        }
+
+        Console.WriteLine(bribes);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine()!.Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()!.Trim());
+
+            List<int> q = Console.ReadLine()!.TrimEnd().Split(' ').ToList().Select(qTemp => Convert.ToInt32(qTemp)).ToList();
+
+            Result.minimumBribes(q);
+        }
+    }
+}
